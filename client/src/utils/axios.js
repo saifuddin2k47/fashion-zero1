69bd8415ee7671577f2c8391556cee8a7a68371c
saifuddin2k47 <saifuddin2k47@gmail.com>
2026-06-29 00:57:13 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "https://fashion-zero-server.onrender.com/api/v1" ,
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://api.fashionzero.in/api/v1" ,
   withCredentials: true,
 });
 
